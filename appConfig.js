dc307a945e997c4fb4ca7454f840fde4bacfefe1
Thiago Telho <96187544+ThiagoTelho@users.noreply.github.com>
2022-09-15 11:39:28 -0300

    Add files via upload

---
+++ appConfig.js
@@ -0,0 +1,22 @@
+
+var appConfig = {
+  serviceBaseUrl: "https://flex.twilio.com/bronze-robin-5276",
+  /*sso: {
+    accountSid: "ACffca6c28c09a263c55824d86300300b4"
+  },*/
+  pluginService: {
+    enabled: true,
+    url: '/plugins',
+  },
+  logLevel: 'info',
+ 
+};
+
+
+
+
+
+
+
+
+
