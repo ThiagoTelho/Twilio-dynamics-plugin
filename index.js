49bcf836b7852371e46422b8ba0490c42c9b412a
Thiago Telho <96187544+ThiagoTelho@users.noreply.github.com>
2022-09-15 11:40:10 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import * as FlexPlugin from '@twilio/flex-plugin';
+import { combineReducers } from 'redux';
 
-import CrmDynamicsPlugin from './CrmDynamicsPlugin';
+import { reduce as CustomTaskListReducer } from './CustomTaskListState';
 
+// Register your redux store under a unique namespace
+export const namespace = 'crm-dynamics-plugin';
 
-FlexPlugin.loadPlugin(CrmDynamicsPlugin);
-
-
-
-
-
+// Combine the reducers
+export default combineReducers({
+  customTaskList: CustomTaskListReducer,
+});
